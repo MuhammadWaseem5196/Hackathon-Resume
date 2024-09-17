9bc54a90f8005344b70951763f6311768ecdd01f
Muhammad Waseem <m.waseem5196@gmail.com>
2024-09-17 20:26:08 +0500

    my project has been completed

---
+++ script.ts
@@ -0,0 +1,14 @@
+const toggleButton = document.getElementById('button') as HTMLButtonElement;
+const skillsDiv = document.getElementById('skills') as HTMLDivElement;
+
+// Add a click event listener to the button
+toggleButton.addEventListener('click', () => {
+    // Toggle the display style of the skills div
+    if (skillsDiv.style.display === 'none') {
+        skillsDiv.style.display = 'block';
+        toggleButton.textContent = 'Hide Skills';  // Change button text
+    } else {
+        skillsDiv.style.display = 'none';
+        toggleButton.textContent = 'Show Skills';  // Change button text
+    }
+});
